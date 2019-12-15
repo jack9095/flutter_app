ac13596d839a5912886478f0c7749d655e03ee38
weall <406981543@qq.com>
2019-12-15 19:17:25 +0800

    json解析成实体

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'shop/index.dart';
 
 // https://blog.csdn.net/ruoshui_t/article/details/95456751   获取状态栏高度 屏幕宽高
-void main() => runApp(ShopWidget());
+void main() {
+  // 强制横屏
+//  SystemChrome.setPreferredOrientations([
+//    DeviceOrientation.landscapeLeft,
+//    DeviceOrientation.landscapeRight
+//  ]);
+
+
+  // 强制竖屏
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
+  runApp(ShopWidget());
+}
 
 class ShopWidget extends StatelessWidget {
   @override
